8e2e560e0f540c127623564a7be6a5e25d5af3a0
rob587 <robertocamm02@yahoo.com>
2026-04-01 23:20:25 +0200

    Add camera and location permissions to Index component

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,32 @@
+import { CameraView, useCameraPermissions } from "expo-camera";
+import * as Location from "expo-location";
+import { useRef, useState } from "react";
 import { Text, View } from "react-native";
 
+type Tappa = {
+  id: string;
+  latitudine: number;
+  longitudine: number;
+  citta: string;
+  fotoUri: string;
+  dataOra: string;
+};
+
 export default function Index() {
+  const [permessoCamera, richiestaPermessoCamera] = useCameraPermissions();
+
+  const [foto, setFoto] = useState<string | null>(null);
+  const cameraRef = useRef<CameraView>(null);
+
+  if (!permessoCamera) return <View />;
+
+  //--------------------
+
+  const [permessoPosizione, richiediPermessoPosizione] =
+    Location.useForegroundPermissions();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
